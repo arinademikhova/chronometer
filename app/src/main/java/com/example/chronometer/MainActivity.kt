85a158b899abc885885896fddbbe7e4bc889f233
arina <arinademikhova@gmail.com>
2026-09-15 17:36:04 +0700

    addTime

--- app/src/main/java/com/example/chronometer/MainActivity.kt
+++ app/src/main/java/com/example/chronometer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.chronometer
 
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -19,10 +20,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +48,22 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChronometerApp() {
+    // Состояние таймера
+    var isRunning by remember { mutableStateOf(false) }
+    var accumulatedTimeMillis by remember { mutableLongStateOf(0L) }
+    var startTimeMillis by remember { mutableLongStateOf(0L) }
+    var displayTimeMillis by remember { mutableLongStateOf(0L) }
+
+    // Фоновый отсчет времени при isRunning = true
+    LaunchedEffect(isRunning, startTimeMillis, accumulatedTimeMillis) {
+        if (isRunning) {
+            while (true) {
+                val currentRealtime = SystemClock.elapsedRealtime()
+                displayTimeMillis = accumulatedTimeMillis + (currentRealtime - startTimeMillis)
+                delay(100)
+            }
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -58,16 +83,22 @@ fun ChronometerApp() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // счетчик
+
+            val totalSeconds = displayTimeMillis / 1000
+            val minutes = totalSeconds / 60
+            val seconds = totalSeconds % 60
+            val formattedTime = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+
+
             Text(
-                text = "00:00",
+                text = formattedTime,
                 fontSize = 64.sp,
                 modifier = Modifier.padding(bottom = 32.dp)
             )
 
             // Кнопка START
             Button(
-                onClick = {  },
+                onClick = { },
                 modifier = Modifier
                     .width(140.dp)
                     .height(48.dp)
@@ -79,7 +110,7 @@ fun ChronometerApp() {
 
             // Кнопка PAUSE
             Button(
-                onClick = {  },
+                onClick = { },
                 modifier = Modifier
                     .width(140.dp)
                     .height(48.dp)
@@ -91,7 +122,7 @@ fun ChronometerApp() {
 
             // Кнопка RESET
             Button(
-                onClick = {  },
+                onClick = { },
                 modifier = Modifier
                     .width(140.dp)
                     .height(48.dp)
@@ -99,5 +130,5 @@ fun ChronometerApp() {
                 Text("RESET", fontSize = 18.sp)
             }
         }
-        }
+    }
 }
